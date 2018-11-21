e38d98f8df771e99f12eb46c81da0dc9ce4c84fa
grishaoganov <grishaoganov19999@gmail.com>
2018-11-22 00:35:44 +0300

    response status fix

--- go-server-server-generated/go/all_api.go
+++ go-server-server-generated/go/all_api.go
@@ -19,8 +19,6 @@ func DeleteAll(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	deleteAll(LOGS)
 	deleteAll(METRICS)
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
 	type ViewData struct {
 		Status string
 	}
